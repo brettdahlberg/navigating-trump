d34443eefd93481544a4f80e012e8884d388e72d
Brett Dahlberg <brettdahlberg@Bretts-MacBook-Pro.local>
2017-05-11 22:50:20 -0400

    css changes for desktop, js changes for button.active

--- financial-disclosures/main.js
+++ financial-disclosures/main.js
@@ -6,11 +6,13 @@ $(document).ready(function () {
 		var filter = button.data("filter");
 
 		if (filter === "reset") {
-			console.log("hello world");
+			console.log("Hello, William.");
 			$("div").removeClass("active");
+			$("button").removeClass("active");
 
 		} else {
 			var button = $(this);
+			$(this).toggleClass("active")
 			var filter = button.data("filter");
 			$("." + filter).toggleClass("active");
 		}
@@ -18,7 +20,7 @@ $(document).ready(function () {
 
 	$(".item").on("click", function (e) {
 
-		console.log("clicked");
+		console.log("photo clicked");
 		var item = $(this);
 		if (item.hasClass("turn")) {
 			item.removeClass("turn");
